a5180b0cca4573c877f69b14f3fc83a07b66ae0c
Matthijs <msin2@me.com>
2016-06-03 12:37:55 +0200

    Homepage setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+		<title>Shopping Cart</title>
+
+		<!-- Semantic UI -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.css">
+	</head>
+
+	<body>
+
+		@yield('content')
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-rc1/jquery.min.js"></script>
+		<!-- Semantic UI -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.1.8/semantic.min.js"></script>
+	</body>
+
+</html>
